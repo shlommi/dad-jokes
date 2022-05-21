e18c1684311303cce15b9444ab5404791b0527b7
shlommi <shlommi@gmail.com>
2022-05-21 20:48:14 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json"
+    }
+  };
+
+  const response = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await response.json();
+
+  jokeEl.innerText = data.joke;
+}
